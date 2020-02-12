6bd6e29d2d98c58ef084b9a7a19342b9c057943c
Hao Ye <lhopitalified@gmail.com>
2020-02-12 15:58:59 -0500

    correct MODIS NDVI based on VI quality data

--- ndvi_data_tests.R
+++ ndvi_data_tests.R
@@ -5,33 +5,145 @@ library(tidyr)
 library(stringr)
 library(ggplot2)
 
-#### MODIS NDVI ----
-products <- mt_products()
-bands <- mt_bands(product = "MOD13Q1")
-dates <- mt_dates(product = "MOD13Q1", lat = 32, lon = -109)
-
 # trying to match up geographical subset and coordinates from 
 # https://github.com/weecology/PortalData/tree/master/NDVI
 
-# portal_ndvi_test <- mt_subset(product = "MOD13Q1", 
-#                               lat = 31.937769, 
-#                               lon = -109.08029, 
-#                               band = "250m_16_days_NDVI", 
-#                               start = "2000-02-18", 
-#                               end = "2012-12-19", 
-#                               km_lr = 8, 
-#                               km_ab = 8, 
+#### MODIS NDVI ----
+# products <- mt_products()
+# bands <- mt_bands(product = "MOD13Q1")
+# dates <- mt_dates(product = "MOD13Q1", lat = 32, lon = -109)
+# 
+# modis_ndvi <- mt_subset(product = "MOD13Q1",
+#                         lat = 31.937769,
+#                         lon = -109.08029,
+#                         band = "250m_16_days_NDVI",
+#                         start = "2000-02-18",
+#                         end = "2018-12-19",
+#                         km_lr = 8,
+#                         km_ab = 8,
+#                         site_name = "portal")
+# 
+# modis_vi_quality <- mt_subset(product = "MOD13Q1",
+#                               lat = 31.937769,
+#                               lon = -109.08029,
+#                               band = "250m_16_days_VI_Quality",
+#                               start = "2000-02-18",
+#                               end = "2018-12-19",
+#                               km_lr = 8,
+#                               km_ab = 8,
 #                               site_name = "portal")
+# 
+# saveRDS(modis_ndvi, file = "data/modis_ndvi_raw.RDS")
+# saveRDS(modis_vi_quality, file = "data/modis_vi_quality.RDS")
 
-portal_ndvi_test <- readRDS("modis_downloaded_ndvi.RDS")
+modis_ndvi_raw <- readRDS("data/modis_ndvi_raw.RDS")
+modis_vi_quality <- readRDS("data/modis_vi_quality.RDS")
 
-modis_ndvi <- portal_ndvi_test %>%
-    mutate(ndvi = value * as.numeric(scale)) %>%
-    group_by(calendar_date) %>%
-    summarize(ndvi = mean(ndvi)) %>%
-    mutate(date = as.Date(calendar_date)) %>%
-    select(date, ndvi) %>%
-    mutate(sensor = "MODIS", source = "MODISTools")
+# this map is taken from Table 5, describing the bit code for the VI Quality 
+# scientific data set, for MOD13A1 / MOD13A1 in the 
+# "MODIS Vegetation Index User's Guide", retrieved from 
+# https://vip.arizona.edu/documents/MODIS/MODIS_VI_UsersGuide_June_2015_C6.pdf
+map_vi_quality <- function(pixel_value)
+{
+    vi_quality_map <- c("VI produced with good quality", 
+                        "VI produced, but check other QA", 
+                        "Pixel produced, but most probably cloudy", 
+                        "Pixel not produced due to other reasons than clouds")
+    
+    vi_usefulness_map <- c("Highest quality", 
+                           "Lower quality", 
+                           "Decreasing quality 2", 
+                           "Decreasing quality 3", 
+                           "Decreasing quality 4", 
+                           "Decreasing quality 5", 
+                           "Decreasing quality 6", 
+                           "Decreasing quality 7", 
+                           "Decreasing quality 8", 
+                           "Decreasing quality 9", 
+                           "Decreasing quality 10", 
+                           "Decreasing quality 11", 
+                           "Lowest quality", 
+                           "Quality so low that it is not useful", 
+                           "L1B data faulty", 
+                           "Not useful for any other reason / not processed")
+    
+    aerosol_quantity_map <- c("Climatology", 
+                              "Low", 
+                              "Intermediate", 
+                              "High")
+    
+    adjacent_cloud_detected_map <- c("No", 
+                                     "Yes")
+    
+    atmosphere_BRDF_correction_map <- c("No", 
+                                        "Yes")
+    
+    mixed_clouds_map <- c("No", 
+                          "Yes")
+    
+    land_water_mask_map <- c("Shallow ocean", 
+                             "Land (Nothing else but land)", 
+                             "Ocean coastlines and lake shorelines", 
+                             "Shallow inland water", 
+                             "Ephemeral water", 
+                             "Deep inland water", 
+                             "Moderate or continental ocean", 
+                             "Deep ocean")
+    
+    possible_snow_ice_map <- c("No", 
+                               "Yes")
+    
+    possible_shadow_map <- c("No", 
+                             "Yes")
+    pixel_bits <- matrix(as.numeric(intToBits(pixel_value)), ncol = 32, byrow = TRUE)
+    out <- data.frame(
+        vi_quality = vi_quality_map[1 + pixel_bits[, 1] + 
+                                        pixel_bits[, 2] * 2], 
+        vi_usefulness = vi_usefulness_map[1 + pixel_bits[, 3] + 
+                                              pixel_bits[, 4] * 2 + 
+                                              pixel_bits[, 5] * 4 + 
+                                              pixel_bits[, 6] * 8], 
+        aerosol_quantity = aerosol_quantity_map[1 + pixel_bits[, 7] + 
+                                                    pixel_bits[, 8] * 2], 
+        adjacent_cloud_detected = adjacent_cloud_detected_map[1 + pixel_bits[, 9]], 
+        atmosphere_BRDF_correction = atmosphere_BRDF_correction_map[1 + pixel_bits[, 10]], 
+        mixed_clouds = mixed_clouds_map[1 + pixel_bits[, 11]], 
+        land_water_mask = land_water_mask_map[1 + pixel_bits[, 12] + 
+                                                  pixel_bits[, 13] * 2 + 
+                                                  pixel_bits[, 14] * 4], 
+        possible_snow_ice = possible_snow_ice_map[1 + pixel_bits[, 15]], 
+        possible_shadow = possible_shadow_map[1 + pixel_bits[, 16]]
+    )
+    
+    return(out)
+}
+
+cols <- c("xllcorner", "yllcorner", "cellsize", "latitude", "longitude", 
+          "start", "end", "modis_date", "calendar_date", "tile")
+modis_vi_quality <- bind_cols(select(modis_vi_quality, cols)
+
+if (NROW(modis_ndvi) == NROW(modis_vi_quality) && 
+    all.equal(modis_ndvi[, cols], modis_vi_quality[, cols], check.attributes = FALSE))
+{
+    modis_ndvi_raw <- bind_cols(modis_ndvi_raw, 
+                                select(modis_vi_quality, -cols))
+    
+    modis_ndvi_processed <- modis_ndvi_raw %>%
+        filter(vi_usefulness == "Highest quality", 
+               aerosol_quantity == "Low", 
+               adjacent_cloud_detected == "No", 
+               mixed_clouds == "No") %>%
+        mutate(ndvi = value * as.numeric(scale)) %>%
+        group_by(calendar_date) %>%
+        summarize(ndvi = mean(ndvi)) %>%
+        mutate(date = as.Date(calendar_date)) %>%
+        select(date, ndvi) %>%
+        mutate(sensor = "MODIS", source = "MODISTools")
+    
+    saveRDS(modis_ndvi_processed, file = "data/modis_ndvi_processed.RDS")
+}
+
+modis_ndvi_processed <- readRDS("data/modis_ndvi_processed.RDS")
 
 #### portalr NDVI ----
 
@@ -64,7 +176,7 @@ landsat_5 <- read_in_GEE_landsat_NDVI("Landsat5_SR_NDVI_Portal_1984_2011.csv")
 landsat_7 <- read_in_GEE_landsat_NDVI("Landsat7_SR_NDVI_Portal_1999_2020.csv")
 
 #### combine datasets and plot ----
-ndvi_dat <- bind_rows(modis_ndvi, 
+ndvi_dat <- bind_rows(modis_ndvi_processed, 
                       gimms_ndvi, 
                       landsat_ndvi, 
                       landsat_5, 
